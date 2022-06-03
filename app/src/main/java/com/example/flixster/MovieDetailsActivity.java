078bddeb25e75813fc5a8c5a10a4e59f8f21710d
Aaron de los Santos <hansaaron_d@yahoo.com>
2022-06-03 13:25:22 -0700

    Initial commit

---
+++ app/src/main/java/com/example/flixster/MovieDetailsActivity.java
@@ -0,0 +1,54 @@
+package com.example.flixster;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.view.menu.MenuView;
+
+import android.content.res.Configuration;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.ImageView;
+import android.widget.RatingBar;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners;
+import com.example.flixster.R;
+import com.example.flixster.models.Movie;
+
+import org.parceler.Parcels;
+
+public class MovieDetailsActivity extends AppCompatActivity {
+    Movie movie;
+
+    // the view objects
+    TextView tvTitle;
+    TextView tvOverview;
+    RatingBar rbVoteAverage;
+    ImageView ivPoster; // added
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_movie_details);
+
+        tvTitle = (TextView) findViewById(R.id.tvTitle);
+        tvOverview = (TextView) findViewById(R.id.tvOverview);
+        rbVoteAverage = (RatingBar) findViewById(R.id.rbVoteAverage);
+        ivPoster = (ImageView) findViewById(R.id.ivPoster); // added
+
+
+        movie = (Movie) Parcels.unwrap(getIntent().getParcelableExtra(Movie.class.getSimpleName()));
+        Log.d("MovieDetailsActivity", String.format("Showing details for '%s'", movie.getTitle()));
+
+        // set the title and overview
+        tvTitle.setText(movie.getTitle());
+        tvOverview.setText(movie.getOverview());
+        String imageUrl; // added
+        imageUrl = movie.getPosterPath(); // added
+        Glide.with(this).load(imageUrl).placeholder(R.drawable.flicks_backdrop_placeholder).into(ivPoster); // added
+
+        // vote average is 0..10, convert to 0..5 by dividing by 2
+        float voteAverage = movie.getVoteAverage().floatValue();
+        rbVoteAverage.setRating(voteAverage / 2.0f);
+    }
+}
